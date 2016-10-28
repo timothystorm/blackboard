d7c4fea45e676ca1764ca486775a5bdd517317e3
Timothy Storm <timothystorm@gmail.com>
2016-10-28 17:32:00 -0600

    simplified session flushing

--- depot/grails-app/controllers/org/storm/depot/AssetController.groovy
+++ depot/grails-app/controllers/org/storm/depot/AssetController.groovy
@@ -1,92 +1,84 @@
 package org.storm.depot
 
 class AssetController {
-   def create() {
-     def assets = Asset.list()
-     [asset: new Asset(), assets:assets, assetOf:assets]
-   }
+  SessionFactory sessionFactory
 
-   def delete() {
-     with{asset -> 
-       removeAssociations(asset)
-       asset.delete(flush:true) 
-       redirect action:'index'
-     }
-   }
+  def create() {
+    def assets = Asset.list()
+    [asset: new Asset(), assets:assets, assetOf:assets]
+  }
+
+  def delete() {
+    with{asset -> 
+      removeAssociations(asset)
+      asset.delete() 
+      redirect action:'index'
+    }
+  }
 
 
-   def edit() {
-     with{asset -> 
-       def assets = Asset.list().findAll{it != asset}
-       [asset:asset, assets:assets, assetOf:assets]
-     }
-   }
+  def edit() {
+    with{asset -> 
+      def assets = Asset.list().findAll{it != asset}
+      [asset:asset, assets:assets, assetOf:assets]
+    }
+  }
 
-   def index() {
-     [assets: Asset.list(params)]
-   }
+  def index() {
+    [assets: Asset.list(params)]
+  }
 
-   def save() {
-     def asset = new Asset(params)
-     upsave(asset)
-     redirect action:'show', id:asset.id
-   }
+  def save() {
+    def asset = new Asset(params)
+    upsave(asset)
+    redirect action:'show', id:asset.id
+  }
 
-   def show() {
-     with{asset -> [asset:asset]}
-   }
+  def show() {
+    with{asset -> [asset:asset]}
+  }
 
-   def update() {
-     with{asset ->
-       removeAssociations(asset)
+  def update() {
+    with{asset ->
+      removeAssociations(asset)
 
-       asset.properties = params
-       upsave(asset)
-       redirect action:'show', id:asset.id
-     }
-   }
+      // capture new asset params and save
+      upsave((asset.properties = params))
+      redirect action:'show', id:asset.id
+    }
+  }
 
-   /**
-    * Utility to update/save an Asset and adding child/parent associations
-    */
-   private def upsave(Asset asset){
-     if(asset?.save(flush:true)) {
-       addAssociations(asset)
-     }
-   }
+  /**
+   * Utility to update/save an Asset and adding child/parent associations
+   */
+  private def upsave(Asset asset){
+    if(asset.save()) addAssociations(asset)
+  }
 
    /** 
     * Utility finds the specified Asset and if found executes the closure with
     * the found Asset as an arg
     */
-   private def with(id='id', Closure closure) {
-     def asset = Asset.get(params[id])
-     if(asset) closure(asset)
-     else {
-       flash.message = "Asset not found"
-       redirect action:'index'
-     }
-   }
+  private def with(id='id', Closure closure) {
+    def asset = Asset.get(params[id])
+    if(asset) closure(asset)
+    else {
+      flash.message = "Asset not found"
+      redirect action:'index'
+    }
+  }
 
-   private def addAssociations(Asset asset){
-     // need to move the assets to tmp to prevent concurrent modification exceptions
-     def tmp = []
-     tmp.addAll(asset.assets)
-     tmp.each{it.addToAssetOf(asset).save(flush:true)}
+  private def addAssociations(Asset asset) {
+    // add cross reference assets
+    asset.assets?.each{it.removeFromAssetOf(asset).save()}
+    asset.assetOf?.each{it.removeFromAssets(asset).save()}
+    sessionFactory?.getCurrentSession()?.flush()
+  }
 
-     tmp = []
-     tmp.addAll(asset.assetOf)
-     tmp.each{it.addToAssets(asset).save(flush:true)}
-   }
-
-   private def removeAssociations(Asset asset){
-     // need to move the assets to tmp to prevent concurrent modification exceptions
-     def tmp = [] 
-     tmp.addAll(asset.assets)
-     tmp.each{it.removeFromAssetOf(asset).save(flush:true)}
-
-     tmp = []
-     tmp.addAll(asset.assetOf)
-     tmp.each{it.removeFromAssets(asset).save(flush:true)}
-   }
+  private def removeAssociations(Asset asset) {
+    // remove cross reference assets
+    asset.assets?.each{it.removeFromAssetOf(asset).save()}
+    asset.assetOf?.each{it.removeFromAssets(asset).save()}
+    sessionFactory?.getCurrentSession()?.flush()
+  }
 }
